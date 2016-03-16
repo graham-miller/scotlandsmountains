1e78d886889bce339379216c1f6df8383159996c
Graham Miller <mail@grahammiller.net>
2016-03-16 08:50:42 +0000

    Changed to use Bing Maps tiles

--- Web/src/components/Map/MapFactory.js
+++ Web/src/components/Map/MapFactory.js
@@ -2,12 +2,10 @@
 
 import '../../../node_modules/leaflet/dist/leaflet.js';
 import '../../../node_modules/leaflet/dist/leaflet.css';
-import 'os-leaflet';
-
 import config from '../../config.js';
 import L from 'leaflet';
-import mapQuest from 'leaflet-providers';
 import $ from 'jquery';
+import BingLayer from './BingLayer';
 
 require.context('../../../node_modules/leaflet/dist/images/', true, /\.(png)$/);
 
@@ -16,18 +14,34 @@ const buildMap = function(htmlElement, center, zoom) {
     L.Icon.Default.imagePath = 'images/leaflet';
 
     var map = new L.Map(htmlElement, {
-        minZoom: 0,
+        minZoom: 6,
         maxZoom: 18,
-        zoom: 7,
+        zoom: zoom,
         center: center,
         zoomControl: false
     });
 
-    var aerialLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    var layer = new BingLayer('http://ak.dynamic.t{s}.tiles.virtualearth.net/comp/ch/{q}?mkt=en-GB&it=G,OS,BX,RL&shading=hill&n=z&og=118&key=' + config.bingMapsKey + '&c4w=1', {
+        subdomains: ['0', '1', '2', '3', '4', '5', '6', '7'],
+        attribution: '&copy; <a href="http://bing.com/maps">Bing Maps</a>',
+        detectRetina: true
     });
-   
-    map.addLayer(aerialLayer);
+
+    map.addLayer(layer);
+
+    // var map = new L.Map(htmlElement, {
+    //     minZoom: 0,
+    //     maxZoom: 18,
+    //     zoom: 7,
+    //     center: center,
+    //     zoomControl: false
+    // });
+
+    // var aerialLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    //     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    // });
+
+    // map.addLayer(aerialLayer);
 
     // var map = new L.Map(htmlElement, {
     //     crs: L.OSOpenSpace.getCRS(),
