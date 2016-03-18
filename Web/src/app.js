b72f15d4e809d27079f1f6eb869e8a45c90a837c
Graham Miller <mail@grahammiller.net>
2016-03-18 16:07:29 +0000

    Added react-router

--- Web/src/app.js
+++ Web/src/app.js
@@ -5,9 +5,11 @@ import 'normalize.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
 
 import AppBarComponent from './components/AppBar/AppBarComponent.js'
 import MapComponent from './components/Map/MapComponent.js'
+import AboutComponent from './components/About/AboutComponent.js'
 import FooterComponent from './components/Footer/FooterComponent.js'
 
 // Needed for onTouchTap, can be removed when react 1.0 is released https://github.com/zilverline/react-tap-event-plugin
@@ -21,10 +23,17 @@ class App extends React.Component {
             <div id="app-component">
                 <AppBarComponent />
                 <MapComponent />
+                {this.props.children}
                 <FooterComponent />
             </div>
         );
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app-container'));
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={null} />
+            <Route path="about" component={AboutComponent} />
+        </Route>
+    </Router>), document.getElementById('app-container'));
---
+++ Web/src/components/About/AboutComponent.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+class AboutComponent extends React.Component {
+    
+    render() {
+        return (
+            <div id="content-container">About goes here!</div>
+        );
+    }
+}
+
+export default AboutComponent;
