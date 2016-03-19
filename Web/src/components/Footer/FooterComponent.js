7148f5c7394b37555dff10623f657c67fb5b4c2b
Graham Miller <mail@grahammiller.net>
2016-03-19 22:33:06 +0000

    Added footer handle

--- Web/src/components/Footer/FooterComponent.js
+++ Web/src/components/Footer/FooterComponent.js
@@ -2,23 +2,34 @@
 
 import React from 'react';
 
+import NavigationExpandMore from 'material-ui/lib/svg-icons/navigation/expand-more';
+
+
 class FooterComponent extends React.Component {
 
-    render() {
+    scrollToFooter() {
+        window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
+    }
 
+    render() {
         return (
             <div id="footer-component">
-                <h2>Scotland's Mountains</h2>
-                <div className="legal">
-                    <p>
-                        Website design / logo &copy; 2016 Graham Miller
-                    </p>
-                    <p>
-                        <a href="#">Terms and conditions</a>&nbsp;&middot;&nbsp;
-                        <a href="#">Copyright</a>&nbsp;&middot;&nbsp;
-                        <a href="#">Privacy policy</a>&nbsp;&middot;&nbsp;
-                        <a href="#">Disclaimer</a>
-                    </p>
+                <div className="handle" onClick={this.scrollToFooter}>
+                    <NavigationExpandMore color={'#e8e8e8'} />
+                </div>
+                <div className="content">
+                    <h2>Scotland's Mountains</h2>
+                    <div className="legal">
+                        <p>
+                            Website design / logo &copy; 2016 Graham Miller
+                        </p>
+                        <p>
+                            <a href="#">Terms and conditions</a>&nbsp;&middot;&nbsp;
+                            <a href="#">Copyright</a>&nbsp;&middot;&nbsp;
+                            <a href="#">Privacy policy</a>&nbsp;&middot;&nbsp;
+                            <a href="#">Disclaimer</a>
+                        </p>
+                    </div>  
                 </div>
             </div>
         );
