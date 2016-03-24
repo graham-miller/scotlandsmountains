05f70ca1f61497fc970434554dab9ac41412ee26
Graham Miller <mail@grahammiller.net>
2016-03-24 22:53:47 +0000

    Updates to footer

--- Web/src/components/Footer/FooterComponent.js
+++ Web/src/components/Footer/FooterComponent.js
@@ -1,22 +1,57 @@
 'use strict';
 
 import React from 'react';
+import $ from 'jquery';
 
 import NavigationExpandMore from 'material-ui/lib/svg-icons/navigation/expand-more';
-
+import NavigationExpandLess from 'material-ui/lib/svg-icons/navigation/expand-less';
 
 class FooterComponent extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {isVisible: false};
+        this.updateHandle = this.updateHandle.bind(this);
+    }
+
+    componentWillMount() {
+        $(window).on('DOMContentLoaded load resize scroll', this.updateHandle);
+    }
+
+    componentDidMount() {
+        this.updateHandle();
+    }
+        
+    updateHandle() {
+        this.setState({isVisible: this.isVisible()});
+    }
+    
+    isVisible() {
+        var el = $('#footer-component .content')[0];
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 && rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
 
     scrollToFooter() {
-        window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
+        $("html, body").animate({scrollTop: $(document).height()});
+    }
+
+    scrollFromFooter() {
+        $("html, body").animate({scrollTop: 0});
     }
 
     render() {
+        
+        const onClick = this.state.isVisible ? this.scrollFromFooter : this.scrollToFooter;
+        const icon = this.state.isVisible ? (<NavigationExpandMore color={'#e8e8e8'} />) : (<NavigationExpandLess color={'#e8e8e8'} />);
+         
         return (
             <div id="footer-component">
-                <div className="handle" onClick={this.scrollToFooter}>
-                    <NavigationExpandMore color={'#e8e8e8'} />
-                </div>
+                <div className="handle" onClick={onClick}>{icon}</div>
                 <div className="content">
                     <h2>Scotland's Mountains</h2>
                     <div className="legal">
