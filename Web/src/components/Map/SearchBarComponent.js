be03fef1f24eb1cc48eb298654ac2f2462353f96
Graham Miller <mail@grahammiller.net>
2016-03-25 14:02:53 +0000

    Initial check-in of search functionality

--- Web/src/components/Map/SearchBarComponent.js
+++ Web/src/components/Map/SearchBarComponent.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 
+import $ from 'jquery';
+
 import Toolbar from 'material-ui/lib/toolbar/toolbar';
 import ToolbarGroup from 'material-ui/lib/toolbar/toolbar-group';
 import IconButton from 'material-ui/lib/icon-button';
@@ -10,17 +12,62 @@ import MenuItem from 'material-ui/lib/menus/menu-item';
 
 class SearchBarComponent extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            timer: null,
+            term: '',
+            results: []
+        };
+        this.handleChanged = this.handleChanged.bind(this);
+    }
+
+    handleChanged(event) {
+        const term = event.target.value;
+        this.setState({ term: term });
+
+        if (term.length > 2) {
+            clearTimeout(this.state.timer);
+            this.setState({
+                timer: setTimeout(() => {
+                    $.get('/api/search/' + encodeURIComponent(term), (data) => {
+                        this.setState({results: data});
+                    });
+                }, 250)    
+            });
+
+        } else {
+            this.setState({ results: [] });
+        }
+    }
+
     render() {
-        
+
         return (
-            <Toolbar style={{paddingTop: '6px', height: '46px'}}>
-                <ToolbarGroup firstChild={true}>
-                    <input
-                        type="search" ref="search"
-                        autoFocus placeholder="Search"
-                        style={{marginLeft:'12px',width:'264px'}} />                  
-                </ToolbarGroup>
-            </Toolbar>
+            <div>
+                <Toolbar style={{ paddingTop: '6px', height: '46px' }}>
+                    <ToolbarGroup firstChild={true}>
+                        <input
+                            type="search" ref="search"
+                            autoFocus placeholder="Search"
+                            style={{ marginLeft: '12px', width: '264px' }}
+                            onChange={this.handleChanged} />
+                    </ToolbarGroup>
+                </Toolbar>
+                {
+                    this.state.results.length > 0 ? (
+                        <div className="search-results">
+                            <div className="scrollable">
+                                <ul>
+                                    {this.state.results.map(function(result) {
+                                        return <li>{result.name}</li>;
+                                    }) }
+                                </ul>
+                            </div>
+                        </div>
+                    ) : null
+                }
+            </div>
         );
     }
 }
