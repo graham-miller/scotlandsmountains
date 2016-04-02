d7f700e4517393c8365f29e6b6a25756297952d0
Graham Miller <mail@grahammiller.net>
2016-04-02 23:17:51 +0100

    Added debug controller

---
+++ Api/Controllers/DebugController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace ScotlandsMountains.Api.Controllers
+{
+    public class DebugController : ApiController
+    {
+        [HttpGet, Route("api/debug")]
+        public dynamic Get()
+        {
+            return new
+            {
+                version = "1.0.0"
+            };
+        }
+    }
+}
