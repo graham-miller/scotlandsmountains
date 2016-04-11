88603152b85409e4ade96b35172576c13c429f8e
Graham Miller <mail@grahammiller.net>
2016-04-11 22:44:54 +0100

    EmailController changed to ContactUsController

--- Api/Models/ContactUsModel.cs
+++ Api/Models/ContactUsModel.cs
@@ -1,18 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using ScotlandsMountains.Api.Validations;
 
 namespace ScotlandsMountains.Api.Models
 {
-    public class EmailModel
+    public class ContactUsModel
     {
         [Required(ErrorMessage = "Email address is required")]
-        [EmailAddressValidation(ErrorMessage = "Invalid email address")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string From { get; set; }
 
         [Required(ErrorMessage = "A subject is required")]
         public string Subject { get; set; }
 
         [Required(ErrorMessage = "A message is required")]
-        public string Message { get; set; }
+        public string Text { get; set; }
     }
 }
