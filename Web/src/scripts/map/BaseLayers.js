70e36a5e973afb1c1b5f54a0a08262dd798c4a58
Graham Miller <mail@grahammiller.net>
2016-04-19 13:48:07 +0100

    Updated to use leaflet-plugins BingLayer

--- Web/src/scripts/map/BaseLayers.js
+++ Web/src/scripts/map/BaseLayers.js
@@ -1,16 +1,41 @@
 'use strict';
 
 import config from '../../config.js';
-import { RoadLayer, MapLayer, AerialLayer } from './BingLayer';
+import '../../../node_modules/leaflet-plugins/layer/tile/Bing.js';
 
 const setBaseLayers = function (map) {
 
     map.MAP_VIEW = 'map';
     map.AERIAL_VIEW = 'aerial';
 
-    let roadLayer = new RoadLayer(config.bingMapsKey);
-    let mapLayer = new MapLayer(config.bingMapsKey);
-    let aerialLayer = new AerialLayer(config.bingMapsKey);
+    const defaults = {
+        mapTypes: {
+            road: 'Road',
+            os: 'OrdnanceSurvey',
+            aerial: 'Aerial',
+        },
+        culture: 'en-GB',
+        attribution: '&copy; 2016 Microsoft Corporation'
+    };
+
+    let roadLayer = new L.BingLayer(config.bingMapsKey, {
+        type: defaults.mapTypes.road,
+        culture: defaults.culture,
+        attribution: defaults.attribution
+    });
+    
+    let mapLayer = new L.BingLayer(config.bingMapsKey, {
+        type: defaults.mapTypes.os,
+        culture: defaults.culture,
+        attribution: defaults.attribution
+    });
+    
+    let aerialLayer = new L.BingLayer(config.bingMapsKey, {
+        type: defaults.mapTypes.aerial,
+        culture: defaults.culture,
+        attribution: defaults.attribution
+    });
+
     let currentLayer = map.MAP_VIEW;
 
     map.getBaseLayer = function () {
