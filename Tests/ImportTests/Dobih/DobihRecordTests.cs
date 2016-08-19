71443e960e9bffd314b76320bce8e28564193deb
Graham Miller <mail@grahammiller.net>
2016-08-19 22:34:20 +0100

    Added DobihRecordTests.cs

---
+++ Tests/ImportTests/Dobih/DobihRecordTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScotlandsMountains.ImportTests.Dobih
+{
+    [TestFixture]
+    public class DobihRecordTests
+    {
+        [Test]
+        public void T()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
