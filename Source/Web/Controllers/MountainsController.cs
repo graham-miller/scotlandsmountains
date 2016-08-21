82da2011979f174dbfc8bccb2d40ac85d36978fe
Graham Miller <mail@grahammiller.net>
2016-08-21 19:11:28 +0100

    Added Web project

---
+++ Source/Web/Controllers/MountainsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ScotlandsMountains.Domain;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class MountainsController : Controller
+    {
+        private DomainRoot _domainRoot;
+
+        public MountainsController(DomainRoot domainRoot)
+        {
+            _domainRoot = domainRoot;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            var mountain = _domainRoot.Mountains.SingleOrDefault(x => x.Id == id);
+
+            if (mountain == null)
+                return NotFound();
+
+            return new ObjectResult(mountain);
+        }
+    }
+}
---
+++ Source/Web/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ScotlandsMountains.Domain;
+using Newtonsoft.Json.Serialization;
+
+namespace Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method tadd services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                .AddJsonFormatters(x => {
+                    x.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    x.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+
+            services.AddSingleton(x => DomainRoot.Load());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
