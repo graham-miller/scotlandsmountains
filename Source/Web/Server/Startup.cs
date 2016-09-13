4ce97dbc33961771dffcac220c8e9e2925a6ec78
Graham Miller <mail@grahammiller.net>
2016-09-13 16:59:35 +0100

    Separate client and server code to different folders in Web project

---
+++ Source/Web/Server/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using ScotlandsMountains.Domain;
+
+namespace ScotlandsMountains.Web.Server
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            //services.AddMvc();
+
+            services
+                .AddMvcCore()
+                .AddJsonFormatters(x => {
+                    x.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    x.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+
+            services.AddSingleton<IDomainRoot>(x => DomainRoot.Load());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app
+                .UseAngular2Routing()
+                .UseStaticFiles()
+                .UseMvc();
+        }
+    }
+
+    public static class Angular2RoutingExtension
+    {
+        public static IApplicationBuilder UseAngular2Routing(this IApplicationBuilder app)
+        {
+            return app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+        }
+    }
+}
