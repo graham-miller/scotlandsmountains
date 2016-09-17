84de1182da6ea0e5f693fb43ed029abdaa47d182
Graham Miller <mail@grahammiller.net>
2016-09-17 20:28:19 +0100

    Changed wwwroot to scotlandsmountains.github.io

--- Source/Web/Client/config/webpack.dev.js
+++ Source/Web/Client/config/webpack.dev.js
@@ -5,22 +5,22 @@ var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-eval-source-map',
 
-  output: {
-    path: helpers.root('../wwwroot'),
-    publicPath: 'http://localhost:3000/',
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js'
-  },
+    output: {
+        path: helpers.root('../scotlandsmountains.github.io'),
+        publicPath: 'http://localhost:3000/',
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
 
-  plugins: [
-    new ExtractTextPlugin('[name].css'),
-    new WebpackNotifierPlugin()
-  ],
+    plugins: [
+      new ExtractTextPlugin('[name].css'),
+      new WebpackNotifierPlugin()
+    ],
 
-  devServer: {
-    historyApiFallback: true,
-    stats: 'minimal'
-  }
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal'
+    }
 });
