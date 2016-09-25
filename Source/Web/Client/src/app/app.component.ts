81641e11fdb028012bbece5cb93809f48afc3f1e
Graham Miller <mail@grahammiller.net>
2016-09-25 20:57:40 +0100

    Removed Tour of Heroes tutorial

--- Source/Web/Client/src/app/app.component.ts
+++ Source/Web/Client/src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-
+    selector: 'sm-app',
     template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
+    <h1>Scotland's Mountains, coming soon</h1>
   `,
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Tour of Heroes';
 }
