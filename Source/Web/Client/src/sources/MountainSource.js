498928f30bdf4993e627fa48ee4b3f5a033065e9
Graham Miller <mail@grahammiller.net>
2016-10-08 08:43:57 +0100

    Temporarily removed response.ok check

--- Source/Web/Client/src/sources/MountainSource.js
+++ Source/Web/Client/src/sources/MountainSource.js
@@ -2,9 +2,9 @@
     search: (term) => {
         return fetch(encodeURI('api/mountains/search/' + term), {mode: 'no-cors', redirect: 'follow'})
             .then((response) => {
-                if (!response.ok) {
-                    throw Error(response.statusText);
-                }
+                //if (!response.ok) {
+                //    throw Error(response.statusText);
+                //}
                 return response.json();
             })
             .catch((error) => {
