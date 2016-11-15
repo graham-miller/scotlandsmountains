105948995e47f3b5329f4f9d6dd16f2fe714692b
Graham Miller <mail@grahammiller.net>
2016-11-15 21:09:30 +0000

    Added layout playground

---
+++ Playground/Layout/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Application Name</h1>
+          <nav>
+            <div>Lists</div>
+            <div>Search</div>
+          </nav>
+        </header>
+        <main>Main</main>
+        <footer>Footer</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
