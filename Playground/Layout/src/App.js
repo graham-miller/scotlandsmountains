f7cbeb8db777f5b455224f4cba4d7df70a7924ad
Graham Miller <mail@grahammiller.net>
2016-11-16 14:19:59 +0000

    Layout playground tinkering

--- Playground/Layout/src/App.js
+++ Playground/Layout/src/App.js
@@ -3,16 +3,37 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div id="app-component">
         <header>
           <h1>Application Name</h1>
           <nav>
-            <div>Lists</div>
-            <div>Search</div>
+            <div className="nav-item">Lists</div>
+            <div className="nav-item">Search</div>
+            <div className="nav-item">Item 3</div>
+            <div className="nav-item">Item 4</div>
+            <div className="nav-item">Item 5</div>
+            <div className="nav-item">Item 6</div>
+            <div className="nav-item">Item 7</div>
+            <div className="nav-item">Item 8</div>
+            <div className="nav-item">Item 9</div>
+            <div className="nav-item">Item 10</div>
           </nav>
         </header>
-        <main>Main</main>
-        <footer>Footer</footer>
+        <main>
+          <div id="map-container">
+            <div id="page-actions">Heading<br/>Sub-heading</div>
+            <div id="map">Map</div>
+          </div>
+          <div id="content-container">
+            Content start
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            Content end
+          </div>
+          <footer>
+            <div className="footer-section legal">Footer 1</div>
+            <div className="footer-section">Footer 2</div>
+          </footer>
+        </main>
       </div>
     );
   }
