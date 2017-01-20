d9b712fb88af4d1ff38a99ded61075c391b82e5d
Graham Miller <graham.miller@hymans.co.uk>
2017-01-20 16:05:31 +0000

    Layout experimentation

--- Playground/Layout/src/App.js
+++ Playground/Layout/src/App.js
@@ -1,71 +1,102 @@
 import React, { Component } from 'react';
-import $ from "jquery";
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.onWindowResize = this.onWindowResize.bind(this);
-    this.resizeMap = this.resizeMap.bind(this);
-  }
-
-  onWindowResize () {
-
-    if(this.onWindowResizeDelay) {
-      clearTimeout(this.onWindowResizeDelay);
-    }
-
-    this.onWindowResizeDelay = setTimeout(this.resizeMap, 100);
-  }
-
-  resizeMap() {
-    // TODO
-  }
-
-  componentDidMount() {
-    $(window).on("resize", this.onWindowResize);
-  }
-
-  componentWillUnmunt() {
-    $(window).on("resize", this.onWindowResize);
-  }
-
   render() {
     return (
-      <div id="app-component">
-        <header>
-          <h1>Application Name</h1>
-          <nav>
-            <div className="nav-item">Lists</div>
-            <div className="nav-item">Search</div>
-            <div className="nav-item">Item 3</div>
-            <div className="nav-item">Item 4</div>
-            <div className="nav-item">Item 5</div>
-            <div className="nav-item">Item 6</div>
-            <div className="nav-item">Item 7</div>
-            <div className="nav-item">Item 8</div>
-            <div className="nav-item">Item 9</div>
-            <div className="nav-item">Item 10</div>
-          </nav>
-        </header>
-        <main>
-          <div id="map-section">
-            <div id="page-actions">Page Actions</div>
-            <div id="map-container">
-              <div id="map">Map</div>
-            </div>
+      <div id="app">
+
+        <div id="header">
+          <h1>Header</h1>
+        </div>
+
+        <div id="map-container">
+          <div id="map-title">Map title</div>
+          <div id="map-padding">
+            <div id="map">Map</div>
           </div>
-          <div id="content-section">
-            Content start
-            <br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />&middot;<br />
-            Content end
-          </div>
-          <footer>
-            <div className="footer-section legal">Footer 1</div>
-            <div className="footer-section">Footer 2</div>
-          </footer>
-        </main>
+        </div>
+
+        <div id="content-container">
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+          Content<br />
+        </div>
+
+        <div id="footer">Footer</div>
+
       </div>
     );
   }
--- Playground/Layout/src/index.js
+++ Playground/Layout/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import "normalize.css/normalize.css";
+import "./reset.scss";
 import "./index.scss";
 
 ReactDOM.render(
