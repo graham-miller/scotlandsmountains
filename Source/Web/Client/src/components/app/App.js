5f6e5772c6ce7fe057d7e932c9104840ce557d74
Graham Miller <mail@grahammiller.net>
2017-01-21 21:20:08 +0000

    Layout refactoring

--- Source/Web/Client/src/components/app/App.js
+++ Source/Web/Client/src/components/app/App.js
@@ -2,20 +2,32 @@ import React, { Component } from "react";
 
 import Title from "./Title";
 import Footer from "./Footer";
-import Routes from "./Routes";
-
-import "./App.scss";
+import Map from "../map/Map";
 
 class App extends Component {
 
     render() {
+        const { map, content } = this.props;
         return (
-            <div id="app-component">
-                <Title />
-                <main>
-                    <Routes/>
-                    <Footer/>
-                </main>
+            <div id="app">
+
+                <Title/>
+
+                <div id="map-section">
+                    <div id="map-title">
+                        {map}
+                    </div>
+                    <Map/>
+                </div>
+
+                <div id="content-section">
+                    <div id="content-wrapper">
+                        {content}
+                    </div>
+                </div>
+
+                <Footer/>
+
             </div>
         );
     }
