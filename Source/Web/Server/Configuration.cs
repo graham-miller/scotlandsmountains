748ee1e5fb7f2a7f11b385c5d59b9713aae688c0
Graham Miller <graham.miller@hymans.co.uk>
2017-01-23 17:02:40 +0000

    Interim commit

---
+++ Source/Web/Server/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScotlandsMountains.Web.Server
+{
+    public class Configuration
+    {
+        public string EmailAddress { get; set; }
+        public MailGunConfiguration MailGun { get; set; }
+    }
+
+    public class MailGunConfiguration
+    {
+        public string BaseUrl { get; set; }
+        public string ApiKey { get; set; }
+        public string DomainName { get; set; }
+    }
+}
---
+++ Source/Web/Server/Models/ContactUsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScotlandsMountains.Web.Server.Models
+{
+    public class ContactUsModel
+    {
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
