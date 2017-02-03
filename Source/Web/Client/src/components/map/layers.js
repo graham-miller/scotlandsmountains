857c942f5ac5db24d6574c2cf648ab12aba91861
Graham Miller <graham.miller@hymans.co.uk>
2017-02-03 15:04:27 +0000

    Updates to bingLayer

--- Source/Web/Client/src/components/map/layers.js
+++ Source/Web/Client/src/components/map/layers.js
@@ -1,4 +1,5 @@
 import L from "leaflet";
+import BingLayer from "./bingLayer";
 
 const mapboxAttribution = "&copy; <a href=\"https://www.mapbox.com/about/maps/\" target=\"_blank\">Mapbox</a>";
 const mapboxToken = "pk.eyJ1IjoiZ3JhaGFtbSIsImEiOiJkNDg0MTMwMjAwZjIxMjNlOTExMzk4YTMxZmM0MWIwMSJ9.9xj78sGiHCxyWzdnAg-jEA";
@@ -17,7 +18,16 @@ const aerialLayer = L.tileLayer(
         maxZoom: 18,
         minZoom: 2
     });
-mapLayer.name = "Aerial";
+aerialLayer.name = "Aerial";
+
+// const osLayer = new BingLayer(
+//     "https://t{subdomain}.ssl.ak.dynamic.tiles.virtualearth.net/comp/ch/{quadkey}?mkt=en-GB&it=G,OS,BX,RL&shading=hill&n=z&og=144&key=AuKhsbA70Bov12p8tCtQr_p04VmODhYMeHBpHAByMzzOK2gbPtrBRQ-YT_U2SfIW&c4w=1", {
+//         subdomains: ["0", "1", "2", "3", "4", "5", "6", "7"],
+//         attribution: "&copy; 2017 Microsoft Corporation image courtesy of Ordnance Survey",
+//         maxZoom: 18,
+//         minZoom: 2
+//     });
+// osLayer.name = "OS";
 
 const layers = [mapLayer, aerialLayer];
 
