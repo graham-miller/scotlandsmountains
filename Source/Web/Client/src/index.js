f8759be3d90bcbc4ff589034a2efc7e008b477a7
Graham Miller <mail@grahammiller.net>
2017-02-04 17:04:21 +0000

    State refactoring

--- Source/Web/Client/src/index.js
+++ Source/Web/Client/src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
-
-import { app } from "./state/index";
+import store from "./state/store";
 import Routes from "./components/app/Routes";
 
 import "normalize.css/normalize.css";
@@ -15,8 +12,6 @@ import "./index.scss";
 
 import injectTapEventPlugin from "react-tap-event-plugin";
 injectTapEventPlugin();
-
-let store = createStore(app, applyMiddleware(thunkMiddleware));
 
 const muiTheme = getMuiTheme({
     palette: {
